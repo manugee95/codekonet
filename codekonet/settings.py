b83bb0720cf5b6feb5e5022bc64b9bea473c0dff
manugee95 <manugeetech@gmail.com>
2022-11-18 15:23:27 +0100

    Ip added

--- codekonet/settings.py
+++ codekonet/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3-&cx-w(x&hrd_1h)8zrkh4!&75kepkmuanlee3f2!fp08#e4p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['54.225.56.119']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'main/static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
